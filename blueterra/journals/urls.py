7057dcc2045bb3606df0a2f51f4dc2b563173635
richurichu <paulsonrichu@gmail.com>
2025-08-12 18:00:05 +0530

    modified serializer

--- blueterra/journals/urls.py
+++ blueterra/journals/urls.py
@@ -6,22 +6,17 @@ from django.urls import path,include
 
 from .views import *
 
-
-
-# router = DefaultRouter()
-# router.register(r'journals', BlogPostViewSet, basename='blogpost')
-
 urlpatterns = [
    path('hello/', hello_api, name='grettings'),
-    path('journals/', BlogPostAPIView.as_view(), name='create-blog'),
-     path('journals/<int:pk>/', BlogPostDetailAPIView.as_view(), name='blog-detail'),
-    path('journal-categories/', BlogCategoryAPIView.as_view(), name='categories'),
+   path('journals/', BlogPostAPIView.as_view(), name='create-blog'),
+   path('journals/<int:pk>/', BlogPostDetailAPIView.as_view(), name='blog-detail'),
+   path('journal-categories/', BlogCategoryAPIView.as_view(), name='categories'),
 
    path('get-journal-categories/', get_journal_categories, name='get_journal_categories'),
    path('get-journals/', get_journals, name='get_journals'),
    path('get-featured-journals/', get_featured_journals, name='get_featured_journals'),
- path('upload-blog-image/', UploadBlogImageView.as_view(), name='upload-blog-image'),
-  path('blog/<slug:slug>/', blog_detail, name='blog-detail'),
+   path('upload-blog-image/', UploadBlogImageView.as_view(), name='upload-blog-image'),
+   path('blog/<slug:slug>/', blog_detail, name='blog-detail'),
 
    path('get-related-journals/', get_related_journals, name='get_related_journals'),
 
